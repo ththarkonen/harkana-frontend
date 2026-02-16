132420351ef61a9b89afafeb2387c8e1fbbbeda4
Teemu Härkönen <teemu.ht.harkonen@outlook.com>
2026-02-16 23:04:34 +0200

    Quick fix for the import.meta type nagging.

--- packages/tools/src/api/data.ts
+++ packages/tools/src/api/data.ts
@@ -17,7 +17,7 @@ var apiParameters = ( project: any ) => {
     var parameters: Record<string, string> = {}
 
     parameters.projectID = project.id
-    parameters.dataType = import.meta.env.VITE_DATA_TYPE
+    parameters.dataType = (import.meta as any).env.VITE_DATA_TYPE
 
     return parameters
 }
@@ -28,7 +28,7 @@ var estimate = async ( project: any, tokenGroupID: string) => {
     var parameters = apiParameters( project )
     parameters.groupID = tokenGroupID
 
-    const base = import.meta.env.VITE_BASE_URL + "/data/estimate"
+    const base = (import.meta as any).env.VITE_BASE_URL + "/data/estimate"
     const url = base + "?" + buildQueryString( parameters )
 
     return await apiFetch<{ success: boolean }>( url )
@@ -39,7 +39,7 @@ var validate = async ( project: any ) => {
 
     var parameters = apiParameters( project )
 
-    const base = import.meta.env.VITE_BASE_URL + "/data/validate"
+    const base = (import.meta as any).env.VITE_BASE_URL + "/data/validate"
     const url = base + "?" + buildQueryString( parameters )
 
     return await apiFetch<{ success: boolean }>( url )
